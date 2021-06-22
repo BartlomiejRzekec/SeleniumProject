ff7dfd04d18d3f6d9e4e12a908fec1e8714e7970
Bartlomiej Rzekec <bartlomiej.rzekec@mapp.com>
2021-06-22 21:55:28 +0200

    first add

---
+++ src/test/java/FirstTest.java
@@ -0,0 +1,14 @@
+
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FirstTest {
+
+    WebDriver driver = new ChromeDriver();
+
+
+
+
+
+}
